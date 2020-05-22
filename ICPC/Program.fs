09d62d726fc5e23cc18e46ea43eb1b6c66cfd883
Kushinga <kushingapfende@gmail.com>
2020-05-22 12:07:50 +0200

    added_Changes

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -2,7 +2,35 @@
 open System
 
 let commaSprinkler input =
-    failwith "Not implemented"
+    //check if input is only one character
+    let length = String.length input
+
+    let checkUpper = (String.exists (fun c -> Char.IsUpper(c)) input)
+
+    //check if an spaces exist to determine if it's more than one word
+    let check_Space = (String.exists (fun c -> c = ' ') input)
+
+    //let check_double_Space = (String.exists (fun c -> c = '  ') input)
+
+    //check if th sentence starts with an invalid character
+    let starts_with_Space =
+        match input.[0] with
+        |' ' -> true
+        |',' -> true
+        |_ -> false
+
+    let ends_with_space =
+           match input.[length-1] with
+           |' ' -> true
+           |_ -> false
+
+    match input with
+    | "" -> None
+    | input when length < 2 -> None
+    |input when check_Space = false -> None
+    |input when starts_with_Space = true -> None
+    |input when ends_with_space = true -> None
+    | _ -> Some(input)
 
 let rivers input =
     failwith "Not implemented"
